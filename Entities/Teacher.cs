34fad289aa1960be672a82cf7f19c0ae05862d9a
Shoshi <as6168779@gmail.com>
2024-11-08 02:00:05 +0200

    first upload

---
+++ Entities/Teacher.cs
@@ -0,0 +1,9 @@
+﻿using HelpClasses;
+
+namespace Entities
+{
+    public class Teacher : Person
+    {
+        public List<Course> courses { get; set; }
+    }
+}
---
+++ HelpClasses/Person.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace HelpClasses
+{
+    public abstract class Person
+    {
+        public int Identity { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public Address Address { get; set; }
+    }
+}
